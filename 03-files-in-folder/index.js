e7f8e315cc9556d474d2001f3851a56efd7ea44a
Elena Naprasnikova <ev.naprasnikova@gmail.com>
2023-05-08 23:27:37 +0300

    feat: solve 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = path.join(__dirname, '/secret-folder');
+
+fs.readdir(dir, {withFileTypes: true}, (err, files) => {
+
+  files.forEach(file => {
+
+    if (file.isFile()) {
+
+      let fileDir = path.join(dir, file.name);
+      let fileName = path.basename(fileDir, path.extname(fileDir));
+      let extName = path.extname(fileDir);
+
+      fs.stat(fileDir, (error, stats) => {
+        let fileSizeBytes = stats.size;
+        let fileSizeKb = fileSizeBytes / 1024;
+        let fileInfo = `${fileName} - ${extName.slice(1)} - ${fileSizeKb}kb`;
+        console.log(fileInfo);
+      })
+
+    }
+
+  })
+});
+
